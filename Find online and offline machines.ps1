d9173f3b56dc1a1ef12675b723ad9560b30a7a20
Ankit Nigam <nigamankit7@gmail.com>
2016-10-12 11:45:05 +0530

    Find online and offline machines - Initial Commit
    Functionality
    1. Opens a pop up to past bulk IPs or Hostnames. If bulk IPs or Hostnames are in clipboard, it gets pasted to the input box automatically. 
    2. Iterate through each machines 
    3. Tells if machine is online or offline.

---
+++ Find online and offline machines.ps1
@@ -0,0 +1,99 @@
+function Read-MultiLineInputBoxDialog([string]$Message, [string]$WindowTitle, [string]$DefaultText)
+{
+    Add-Type -AssemblyName System.Drawing
+    Add-Type -AssemblyName System.Windows.Forms
+     
+    # Create the Label.
+    $label = New-Object System.Windows.Forms.Label
+    $label.Location = New-Object System.Drawing.Size(10,10) 
+    $label.Size = New-Object System.Drawing.Size(280,20)
+    $label.AutoSize = $true
+    $label.Text = $Message
+     
+    # Create the TextBox used to capture the user's text.
+    $textBox = New-Object System.Windows.Forms.TextBox 
+    $textBox.Location = New-Object System.Drawing.Size(10,40) 
+    $textBox.Size = New-Object System.Drawing.Size(575,200)
+    $textBox.AcceptsReturn = $true
+    $textBox.AcceptsTab = $false
+    $textBox.Multiline = $true
+    $textBox.ScrollBars = 'Both'
+    $textBox.Text = $DefaultText
+     
+    # Create the OK button.
+    $okButton = New-Object System.Windows.Forms.Button
+    $okButton.Location = New-Object System.Drawing.Size(415,250)
+    $okButton.Size = New-Object System.Drawing.Size(75,25)
+    $okButton.Text = "OK"
+    $okButton.Add_Click({ $form.Tag = $textBox.Text; $form.Close() })
+     
+    # Create the Cancel button.
+    $cancelButton = New-Object System.Windows.Forms.Button
+    $cancelButton.Location = New-Object System.Drawing.Size(510,250)
+    $cancelButton.Size = New-Object System.Drawing.Size(75,25)
+    $cancelButton.Text = "Cancel"
+    $cancelButton.Add_Click({ $form.Tag = $null; $form.Close() })
+     
+    # Create the form.
+    $form = New-Object System.Windows.Forms.Form 
+    $form.Text = $WindowTitle
+    $form.Size = New-Object System.Drawing.Size(610,320)
+    $form.FormBorderStyle = 'FixedSingle'
+    $form.StartPosition = "CenterScreen"
+    $form.AutoSizeMode = 'GrowAndShrink'
+    $form.Topmost = $True
+    $form.AcceptButton = $okButton
+    $form.CancelButton = $cancelButton
+    $form.ShowInTaskbar = $true
+     
+    # Add all of the controls to the form.
+    $form.Controls.Add($label)
+    $form.Controls.Add($textBox)
+    $form.Controls.Add($okButton)
+    $form.Controls.Add($cancelButton)
+     
+    # Initialize and show the form.
+    $form.Add_Shown({$form.Activate()})
+    $form.ShowDialog() > $null   # Trash the text of the button that was clicked.
+     
+    # Return the text that the user entered.
+    return $form.Tag
+}
+
+function fastping{
+  [CmdletBinding()]
+  param(
+  [String]$computername = "127.0.0.1",
+  [int]$delay = 100
+  )
+
+  $ping = new-object System.Net.NetworkInformation.Ping
+  # see http://msdn.microsoft.com/en-us/library/system.net.networkinformation.ipstatus%28v=vs.110%29.aspx
+  try {
+    if ($ping.send($computername,$delay).status -ne "Success") {
+      return $false;
+    }
+    else {
+      return $true;
+    }
+  } catch {
+    return $false;
+  }
+}
+$clipboard_text = Get-Clipboard
+$multiLineText = Read-MultiLineInputBoxDialog -Message "Please enter some text. It can be multiple lines" -WindowTitle "Multi Line Example" -DefaultText $clipboard_text 
+if ($multiLineText -eq $null) { Write-Host "You clicked Cancel" }
+else 
+    {  
+        $multiLineText.replace("`r","").Split() | ForEach {
+        if($_){
+        if((fastping -computername $_) -eq $true)
+        {write-host "$_ is online" -ForegroundColor Green}
+        else
+        {write-host "$_ is offline" -ForegroundColor Red}}    
+        } 
+    }
+
+Write-Host "Press any key to continue ..."
+
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") 
